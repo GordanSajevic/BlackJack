d2726ed6bf61bc99ccd08001e9a2a526550cd138
Gordan Sajević <gsajevic@hotmail.com>
2014-12-16 16:51:04 +0100

    Create Spil.java

---
+++ Spil.java
@@ -0,0 +1,66 @@
+
+public class Spil {
+
+	private Card[] spil;
+	int brojac;
+	
+	/**
+	 * Konstruktor koji dodjeljuje špilu sve karte, a zatim poziva funkciju za miješanje karti
+	 */
+	
+	public Spil()
+	{
+		spil = new Card[52];
+		
+		for (int i=0; i<52; i++)
+		{
+			spil[i] = new Card(i);
+		}
+		mijesanjeKarti();
+	}
+	
+	/**
+	 * Funkcija koja vraća string sa svim članovima špila
+	 */
+	
+	public String toString()
+	{
+		String spilKarata = "";
+		for (int i=0; i<52; i++)
+		{
+			spilKarata += spil[i].toString() + "\n";
+		}
+		return spilKarata;
+	}
+	
+	/**
+	 * Funkcija koja simulira miješanje karti. Kartama se dodjeljuju novi indeksi, i to se ponavlja 52 puta
+	 */
+	
+	private void mijesanjeKarti()
+	{
+		for (int i=0; i<spil.length; i++)
+		{
+			int index1 = (int)(Math.random() * 52);
+			int index2 = (int)(Math.random() * 52);
+			Card karta = spil[index1];
+			spil[index1] = spil[index2];
+			spil[index2] = karta;
+		}
+	}
+	
+	/**
+	 * Funkcija koja vraća kartu iz promiješanog špila
+	 * @return
+	 */
+	
+	public Card getNext()
+	{
+		if (brojac >= 52)
+		{
+			throw new IllegalArgumentException("NEEEE!");
+		}
+		return spil[brojac++];
+	}
+	
+}

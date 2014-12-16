19038db142ec04da2da7af29a9ca5669675dbfe5
Gordan Sajević <gsajevic@hotmail.com>
2014-12-16 20:02:04 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,16 +2,19 @@
 public class Test {
 
 	public static void main(String[] args) {
-		
 		Spil spil = new Spil();
-		
-		System.out.println(spil.toString());
-		Card karta = spil.getNext();
-		System.out.println("Prva karta iz špila: " + karta.toString());
-		karta = spil.getNext();
-		System.out.println("Druga karta iz špila: " + karta.toString());
-		karta = spil.getNext();
-		System.out.println("Treća karta iz špila: " + karta.toString());
+		Ruka ruka = new Ruka();
+		String unos = "da";
+		do
+		{
+			Card karta = spil.getNext();
+			System.out.println("Karta je: " + karta);
+			ruka.dodajKartu(karta);
+			System.out.println("Da li želite još jednu kartu?");
+			unos = TextIO.getln();
+			
+		}while (unos.equals("da"));
+		System.out.println("Suma je: " + ruka.vratiSumu());
 	}
 
 }
